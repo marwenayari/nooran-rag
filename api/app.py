7ec1296ec7059899366748a51c51dbe23a20c13d
Marwen Ayari <marwenayarimail@gmail.com>
2024-11-06 00:34:13 +0100

    refactor: adjust structure for vercel

--- api/app.py
+++ api/app.py
@@ -4,14 +4,17 @@ from model import setup_watsonx_model, get_watsonx_response
 from utils import setup_chromadb, search_similar_documents, construct_prompt
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv('.env', override=True)
 
 app = FastAPI()
 
 # Setup the IBM Watsonx AI model
 model = setup_watsonx_model()
 
-# Setup ChromaDB collection
-collection_name = os.getenv("CHROMADB_COLLECTION_NAME", "nooran_x_allam")
+# Setup ChromaDB collection with persistence
+collection_name = os.getenv("CHROMADB_COLLECTION_NAME", "default_collection")
 persist_directory = os.getenv("CHROMADB_PERSIST_DIR", "./chroma_db")
 collection = setup_chromadb(collection_name, persist_directory)
 
@@ -19,13 +22,11 @@ class QueryRequest(BaseModel):
     query: str
 
 @app.post("/generate")
-def generate_response(request: QueryRequest):
+async def generate_response(request: QueryRequest):
     query = request.query
     try:
         similar_docs = search_similar_documents(collection, query)
-        print(f"Similar documents: {similar_docs}")
         prompt = construct_prompt(query, similar_docs)
-        print(f"Constructed prompt: {prompt}")
         response = get_watsonx_response(model, prompt)
         return {"response": response}
     except Exception as e:
